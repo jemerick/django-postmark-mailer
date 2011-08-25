930dadfc02ab66fea72e92337bcc447c8a5a431a
Jason Emerick <jemerick@gmail.com>
2011-08-25 10:50:05 -0400

    initial commit setting up project framework

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+setup(
+    name="django-postmark-mailer",
+    version=__import__("postmark_mailer").__version__,
+    description="A reusable Django app for queuing the sending of email via postmarkapp.com batch messaging API",
+    long_description=open("docs/usage.txt").read(),
+    author="Jason Emerick",
+    author_email="jemerick@gmail.com",
+    url="",
+    packages=[
+        "postmark_mailer",
+        "postmark_mailer.management",
+        "postmark_mailer.management.commands",
+    ],
+    package_dir={"postmark_mailer": "postmark_mailer"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
